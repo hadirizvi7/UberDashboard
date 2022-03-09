adfab8070ae438d8d2f49ded32e45444ceb4360c
Syed Hadi Rizvi <shr79@pitt.edu>
2022-03-09 14:32:46 -0500

    venv setup

---
+++ app.py
@@ -0,0 +1,11 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
