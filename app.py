d6ba0e213b7d75076a745207ad402fdc2d353333
Syed Hadi Rizvi <shr79@pitt.edu>
2022-03-10 15:08:34 -0500

    imported data

--- app.py
+++ app.py
@@ -7,5 +7,47 @@ import pandas as pd
 
 app = Dash(__name__)
 
+mapbox_access_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG50amg0dnJieG4ifQ.Zme1-Uzoi75IaFbieBDl3A"
+app = dash.Dash(__name__)
+app.title = "Uber NYC Analytics"
+server = app.server
+
+april_url = 'https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-apr14.csv'
+april_df = pd.read_csv(april_url, dtype=object)
+
+may_url = 'https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-may14.csv'
+may_df = pd.read_csv(may_url, dtype=object)
+
+june_url = 'https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-jun14.csv'
+june_df = pd.read_csv(june_url, dtype=object)
+
+july_url = 'https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-jul14.csv'
+july_df = pd.read_csv(july_url, dtype=object)
+
+aug_url = 'https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-aug14.csv'
+aug_df = pd.read_csv(aug_url, dtype=object)
+
+sep_url = 'https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-sep14.csv'
+sep_df = pd.read_csv(sep_url, dtype=object)
+
+frames = [april_df, may_df, june_df, july_df, aug_df, sep_df]
+df = pd.concat(frames)
+
+list_of_locations = {
+    "Madison Square Garden": {"lat": 40.7505, "lon": -73.9934},
+    "Yankee Stadium": {"lat": 40.8296, "lon": -73.9262},
+    "Empire State Building": {"lat": 40.7484, "lon": -73.9857},
+    "New York Stock Exchange": {"lat": 40.7069, "lon": -74.0113},
+    "JFK Airport": {"lat": 40.644987, "lon": -73.785607},
+    "Grand Central Station": {"lat": 40.7527, "lon": -73.9772},
+    "Times Square": {"lat": 40.7589, "lon": -73.9851},
+    "Columbia University": {"lat": 40.8075, "lon": -73.9626},
+    "United Nations HQ": {"lat": 40.7489, "lon": -73.9680},
+}
+
+df["Date/Time"] = pd.to_datetime(df["Date/Time"], format="%m/%d/%Y %H:%M:%S")
+df.index = df["Date/Time"]
+df.drop("Date/Time", 1, inplace=True)
+
 if __name__ == '__main__':
     app.run_server(debug=True)
